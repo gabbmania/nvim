34fced95269eb00e89bd362cf09893cd29c9bc4e
Gabriel Mania <mania.gabb@gmail.com>
2025-11-20 12:10:26 +0800

    Added toggle keymap for opening built-in terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set({ "n", "t" }, "<C-/>", function()
+  require("snacks").terminal.toggle(nil, {
+    cwd = vim.fn.getcwd(),
+    win = {
+      style = "float",
+    },
+  })
+end, { desc = "Toggle Terminal" })
+
+vim.keymap.set({ "n", "t" }, "<C-_>", function()
+  require("snacks").terminal.toggle()
+end, { desc = "Toggle Terminal" })
