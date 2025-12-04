c1ad8b00ed96e8ad9c075845d4749c3479f7bbf1
Gabriel Mania <mania.gabb@gmail.com>
2025-12-04 12:41:14 +0800

    Automatically activate Python virtual environment in terminal

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,34 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  desc = "Auto-activate virtualenv in terminal",
+  pattern = "*",
+  callback = function()
+    local venv_names = { ".venv", "venv", "env", ".env" }
+    local venv_name = nil
+
+    for _, name in ipairs(venv_names) do
+      if vim.fn.isdirectory(name) == 1 then
+        venv_name = name
+        break
+      end
+    end
+
+    if not venv_name then
+      return
+    end
+
+    local cmd = ""
+    if vim.fn.has("win32") == 1 then
+      cmd = ".\\" .. venv_name .. "\\Scripts\\Activate.ps1"
+    else
+      cmd = "source " .. venv_name .. "/bin/activate"
+    end
+
+    vim.defer_fn(function()
+      vim.api.nvim_chan_send(vim.b.terminal_job_id, cmd .. "\n")
+    end, 100)
+  end,
+})
