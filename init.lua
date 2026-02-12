4720f0339e0b3a52acedad2b9cac732cd6e0ff5f
Gabriel Mania <mania.gabb@gmail.com>
2026-02-12 16:01:33 +0800

    Switch clipboard protocol to `xclip`

--- init.lua
+++ init.lua
@@ -1,2 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.g.clipboard = {
+  name = "xclip-wayland-bridge",
+  copy = {
+    ["+"] = "xclip -selection clipboard",
+    ["*"] = "xclip -selection primary",
+  },
+  paste = {
+    ["+"] = "xclip -selection clipboard -o",
+    ["*"] = "xclip -selection primary -o",
+  },
+  cache_enabled = 1,
+}
+
+vim.opt.clipboard = "unnamedplus"
